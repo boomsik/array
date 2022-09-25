c5e1ca018a02e3a21ae2f28f5ec45fbbe1bb031b
Viktor <pozdnyakov151994@gmail.com>
2022-09-25 21:30:47 +0300

    second commit

--- script.js
+++ script.js
@@ -25,23 +25,84 @@ var furryAndScalyAnimals = furryAnimals.concat(scalyAnimals);
 console.log(furryAndScalyAnimals);
 
 
+// var listBooksViktor = ["Harry Potter", "Lord of the Rings","Sword and fire"];
+// console.log(listBooksViktor.length);
+// console.log(listBooksViktor[0]);
+// listBooksViktor.push("481 degrees Fahrenheit");
+// console.log(listBooksViktor.length);
+// listBooksViktor.unshift("dog's heart");
+// console.log(listBooksViktor.length);
+// console.log(listBooksViktor);
+
+// var listBooksJulia = ["Alchemist","Dracula","Alice in wonderland"];
+// console.log(listBooksJulia.length);
+// console.log(listBooksJulia[0]);
+// listBooksJulia.push("The Master and Margarita");
+// console.log(listBooksJulia.length);
+// listBooksJulia.unshift("How to Stop Being a Sheep");
+// console.log(listBooksJulia.length);
+// console.log(listBooksJulia);
+
+// var listViktorAndJulia = listBooksViktor.concat(listBooksJulia);
+// console.log(listViktorAndJulia);
+
+
+
+
 var listBooksViktor = ["Harry Potter", "Lord of the Rings","Sword and fire"];
 console.log(listBooksViktor.length);
 console.log(listBooksViktor[0]);
 listBooksViktor.push("481 degrees Fahrenheit");
+let firstAddedBookViktor = listBooksViktor[3];
 console.log(listBooksViktor.length);
 listBooksViktor.unshift("dog's heart");
+let secondAddedBookViktor = listBooksViktor[0];
 console.log(listBooksViktor.length);
 console.log(listBooksViktor);
+console.log(firstAddedBookViktor);
+console.log(secondAddedBookViktor);
 
 var listBooksJulia = ["Alchemist","Dracula","Alice in wonderland"];
 console.log(listBooksJulia.length);
 console.log(listBooksJulia[0]);
 listBooksJulia.push("The Master and Margarita");
+let firstAddedBookJulia = listBooksJulia[3];
 console.log(listBooksJulia.length);
 listBooksJulia.unshift("How to Stop Being a Sheep");
+let secondAddedBookJulia = listBooksJulia[0];
 console.log(listBooksJulia.length);
 console.log(listBooksJulia);
+console.log(firstAddedBookJulia);
+console.log(secondAddedBookJulia);
 
-var listViktorAndJulia = listBooksViktor.concat(listBooksJulia);
-console.log(listViktorAndJulia);
+var addedBooksViktorAndJulia = [firstAddedBookViktor,secondAddedBookViktor,firstAddedBookJulia,secondAddedBookJulia];
+console.log(addedBooksViktorAndJulia);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var pigs = ["pig1","pig2","pig3"];
+pigs.push("pig5");
+var pigVlad = pigs[3];
+console.log(pigVlad);
+
+
+
+
+var pigindex = pigs.push("pig4");
+var addedPig = pigs[pigindex];
+console.log(pigindex);
+console.log(pigs);
+console.log(addedPig);
